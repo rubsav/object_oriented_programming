798b0ba1ed5861ed20dd066b0d9df21a4b6f7842
Ruben Avila <Ruavilaa@hotmail.com>
2014-10-23 14:03:15 -0400

    Created a parent class called Person that contains attribute name. Student and Instructor inherit a greeting method. Finally created an instance of Instructor and Student with the names Chris and Cristina respectively

--- people.rb
+++ people.rb
@@ -1,10 +1,22 @@
-class Student
+class Person # 4 parent class that contains attribute name
+	def initialize(name)
+		@name = name
+	end
+	def greeting
+		puts "Hi, my name is #{name}" #5 common greeting method
+	end
+end
+
+class Student < Person 
 	def learn
 		puts "I get it!"
 	end
 end
-class Instructor
+class Instructor < Person
 	def teach
 		puts "Everything in Ruby is an Object"
 	end
 end
+
+Chris = Instructor.new("Chris")#6
+Cristina = Student.new("Cristina")#7
