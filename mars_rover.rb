4fc9ec62c29402a2de8f3cc48bba09bb7951de5b
Ruben Avila <Ruavilaa@hotmail.com>
2014-10-23 16:20:12 -0400

    created a method move that gives movement instructions

--- mars_rover.rb
+++ mars_rover.rb
@@ -15,7 +15,21 @@ class Rover
 	puts "Input instructions (M, L, R):"
 	@instructions = get.chomp
 	end
+
+	def move
+		if (@direction == "N") 
+			@y = y + 1
+		elsif (@direction == "S")
+			@x = y - 1
+		elsif (@direction == "W")
+			@x = x - 1
+		else (@direction == "E")
+			@x = x + 1
+		end
+	end		
 end
+
+	
 
 
 
