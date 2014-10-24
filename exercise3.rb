bad15e401690145b0ac827e63cf5071d488c70e8
Ruben Avila <Ruavilaa@hotmail.com>
2014-10-24 09:28:18 -0400

    included each receipt and converted it into an array. Also included several values for sales import and tax for both

---
+++ exercise3.rb
@@ -0,0 +1,12 @@
+class SalesTexes
+  
+ RECEIPT1 = [ "1 book at 12.49", "1 music CD at 14.99", "1 chocolate bar at 0.85" ]
+ RECEIPT2 = ["1 imported box of chocolates at 10.00", "1 imported bottle of perfume at 47.50"]
+ RECEIPT3 = [ "1 imported bottle of perfume at 27.99", "1 bottle of perfume at 18.99", "1 packet of headache pills at 9.75", "1 box of imported chocolates at 11.25"]
+  
+ TAX_EXCEPT_ITEM = ["chocolates","book","pills","chocolate"]
+  
+ IMPORT_TAX = 0.05
+ SALES_TAX = 0.10
+ BOTH_TAX = 0.15
+ CENTS_ROUNDING_MULTIPLIER = 20.0
